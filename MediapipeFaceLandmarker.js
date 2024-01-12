050b948cb8914b88197109ea39c5cbbccbc0549c
TiboMessiaen <tibo.messiaen@student.howest.be>
2024-01-12 17:05:11 +0100

    feat: Injecting eyeblinking code into page

---
+++ MediapipeFaceLandmarker.js
@@ -0,0 +1,118 @@
+import vision from 'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3';
+// import vision from './TaskVision.js';
+const { FaceLandmarker, FilesetResolver, DrawingUtils } = vision;
+const videoBlendShapes = document.getElementById('video-blend-shapes');
+let faceLandmarker;
+let runningMode = 'IMAGE';
+// let enableWebcamButton;
+let webcamRunning = false;
+// Before we can use HandLandmarker class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment to
+// get everything needed to run.
+async function createFaceLandmarker() {
+  const filesetResolver = await FilesetResolver.forVisionTasks(
+    'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm',
+  );
+  faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+    baseOptions: {
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+      delegate: 'GPU',
+    },
+    outputFaceBlendshapes: true,
+    runningMode,
+    numFaces: 1,
+  });
+}
+createFaceLandmarker();
+
+const video = document.getElementById('webcam');
+const canvasElement = document.getElementById('output_canvas');
+const canvasCtx = canvasElement.getContext('2d');
+// Check if webcam access is supported.
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+// If webcam supported, add event listener to button for when user
+// wants to activate it.
+if (hasGetUserMedia()) {
+  //   enableWebcamButton = document.getElementById('webcamButton');
+  //   enableWebcamButton.addEventListener('click', enableCam);
+  setTimeout(enableCam, 600);
+  console.log('Enabling webcam');
+  //   enableCam();
+} else {
+  console.warn('getUserMedia() is not supported by your browser');
+}
+// Enable the live webcam view and start detection.
+function enableCam() {
+  console.log('enableCam function called');
+  if (!faceLandmarker) {
+    console.log('Wait! faceLandmarker not loaded yet.');
+    return;
+  }
+  if (webcamRunning === true) {
+    webcamRunning = false;
+    // enableWebcamButton.innerText = 'ENABLE PREDICTIONS';
+  } else {
+    webcamRunning = true;
+    // enableWebcamButton.innerText = 'DISABLE PREDICTIONS';
+  }
+  // getUsermedia parameters.
+  const constraints = {
+    video: true,
+  };
+  // Activate the webcam stream.
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadeddata', predictWebcam);
+  });
+}
+let lastVideoTime = -1;
+let results = undefined;
+const drawingUtils = new DrawingUtils(canvasCtx);
+async function predictWebcam() {
+  video.style.display = 'none';
+  // Now let's start detecting the stream.
+  if (runningMode === 'IMAGE') {
+    runningMode = 'VIDEO';
+    await faceLandmarker.setOptions({ runningMode: runningMode });
+  }
+  let startTimeMs = performance.now();
+  if (lastVideoTime !== video.currentTime) {
+    lastVideoTime = video.currentTime;
+    results = faceLandmarker.detectForVideo(video, startTimeMs);
+  }
+  drawBlendShapes(videoBlendShapes, results.faceBlendshapes);
+  // Call this function again to keep predicting when the browser is ready.
+  if (webcamRunning === true) {
+    window.requestAnimationFrame(predictWebcam);
+  }
+}
+
+let OldValueEyeBlinkLeft = 0;
+let OldValueEyeBlinkRight = 0;
+
+function drawBlendShapes(el, blendShapes) {
+  if (!blendShapes.length) {
+    return;
+  }
+  // print the categorie score of the categorie with the categorie name eyeBlinkLeft
+  const valueEyeBlinkLeft = blendShapes[0].categories
+    .map((shape) =>
+      shape.categoryName === 'eyeBlinkLeft' ? shape.score : null,
+    )
+    .filter((shape) => shape !== null)[0];
+  const valueEyeBlinkRight = blendShapes[0].categories
+    .map((shape) =>
+      shape.categoryName === 'eyeBlinkRight' ? shape.score : null,
+    )
+    .filter((shape) => shape !== null)[0];
+  if (valueEyeBlinkLeft > 0.5 && OldValueEyeBlinkLeft < 0.5) {
+    console.log('eyeBlinkLeft detected');
+  }
+  if (valueEyeBlinkRight > 0.5 && OldValueEyeBlinkRight < 0.5) {
+    console.log('eyeBlinkRight detected');
+  }
+  OldValueEyeBlinkLeft = valueEyeBlinkLeft;
+  OldValueEyeBlinkRight = valueEyeBlinkRight;
+}
