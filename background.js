3a49a10bd68536b09ac1c48464ab65c2490ae5a6
TiboMessiaen <tibo.messiaen@student.howest.be>
2024-01-17 21:17:05 +0100

    feat: saving localstorage data from extension to active page on startup

--- background.js
+++ background.js
@@ -17,12 +17,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         chrome.scripting.executeScript({
           target: { tabId: tabId },
           function: (action) => {
-
             // Add Custom CSS - Function
             const Add_Custom_CSS = (css) =>
-            (document.head.appendChild(
-              document.createElement('style'),
-            ).innerHTML = css);
+              (document.head.appendChild(
+                document.createElement('style'),
+              ).innerHTML = css);
 
             // Create Custom Element - Function
             function Create_Custom_Element(tag, attr_tag, attr_name, value) {
@@ -115,7 +114,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   }
 
-
   // *** Storage ***
   // Set data in storage
   if (request.action === 'storeDataToChromeStorage') {
@@ -152,10 +150,48 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // Log action
     console.log('getDataFromChromeStorage');
 
+    // Set data in localstorage from current active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      if (tabs.length > 0) {
+        const tabId = tabs[0].id;
+
+        // Send a message to the content script to set data in localstorage
+        chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          function: (key, value) => {
+            // Set data in localstorage
+            console.log('key', key);
+            console.log('value', value);
+            localStorage.setItem(key, value);
+          },
+          args: [request.key, request.value],
+        });
+      }
+    });
+
     // Get data from storage
     chrome.storage.sync.get([request.key], function (result) {
       console.log('Value currently is ' + result[request.key]);
       console.log(result[request.key]);
+
+      // Set data in localstorage from current active tab
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (tabs.length > 0) {
+          const tabId = tabs[0].id;
+
+          // Send a message to the content script to set data in localstorage
+          chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            function: (key, value) => {
+              // Set data in localstorage
+              console.log('key', key);
+              console.log('value', value);
+              localStorage.setItem(key, value);
+            },
+            args: [request.key, result[request.key]],
+          });
+        }
+      });
 
       // Send response
       sendResponse({ message: result[request.key] });
